c535fe2aa30e2a97fb908cd7608db3d9965409fd
IakimovaIulia <yulchix@no.mail.ru>
2024-03-25 17:26:07 +0300

    Add login page checks to test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.common.by import By
+from pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -14,4 +14,6 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, link)
+    login_page .should_be_login_page()
 
