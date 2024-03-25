43cff6d34b9eee67856069e162544cecc3e8bb73
IakimovaIulia <yulchix@no.mail.ru>
2024-03-25 17:19:48 +0300

    Add check that we actually went to the login page

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, (f"подстрока 'login' отсутcтвует в текущем "
+                                                     f"url {self.browser.current_url} браузера")
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "форма логина не найдена на странице"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "форма регистрации не найдена на странице"
