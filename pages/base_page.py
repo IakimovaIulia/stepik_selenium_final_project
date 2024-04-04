74499754d5984450a65230b0654266f9e159215d
IakimovaIulia <yulchix@no.mail.ru>
2024-04-04 15:34:22 +0300

    add negative checks for element  absence

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,8 @@
 import math
 
-from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
@@ -19,7 +21,22 @@ class BasePage:
             return False
         return True
 
-    from selenium.common.exceptions import NoAlertPresentException  # в начале файла
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
--- pages/product_page.py
+++ pages/product_page.py
@@ -24,3 +24,11 @@ class ProductPage(BasePage):
         expected_message = "Your basket total is now " + price
         assert expected_message in message, \
             f"Expected message: {expected_message} has not been found in message: {message}"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT_NAME), \
+            "Success message is presented, but should not be"
+
+    def should_success_message_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT_NAME), \
+            "Success message is presented, but should have disappeared"
