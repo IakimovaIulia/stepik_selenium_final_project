7ad08397b0b7ea633a6f859b5f224e920d4145e3
IakimovaIulia <yulchix@no.mail.ru>
2024-04-10 15:56:55 +0300

    add test_guest_cant_see_product_in_basket*

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,10 @@
 from selenium.webdriver.common.by import By
 
 
-class BasePageLocators():
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_BTN = (By.CSS_SELECTOR, '.basket-mini a.btn-default')
 
 
 class MainPageLocators:
@@ -21,3 +22,8 @@ class ProductPageLocators:
     ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, "button.btn-add-to-basket")
     SUCCESS_MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages .alert-success div.alertinner")
     SUCCESS_MESSAGE_PRODUCT_PRICE = (By.CSS_SELECTOR, "#messages .alert-info div.alertinner > p")
+
+
+class BasketPageLocators:
+    BASKET_ITEMS = (By.CSS_SELECTOR, '.basket_summary#basket_formset')
+    MESSAGE_BASKET_IS_EMPTY = (By.CSS_SELECTOR, '.content #content_inner>p')
--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
@@ -17,3 +18,12 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, link)
     login_page .should_be_login_page()
 
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, link)
+    basket_page.should_not_be_items_in_basket()
+    basket_page.should_be_message_empty_basket()
